65e5dc09e4b46bc3318f87702e69040761dacc93
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-10-17 15:03:24 +0300

    Add files via upload

---
+++ odev4-847/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace odev4_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.btnUygula = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(80, 77);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(75, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "dark mode";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(80, 112);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(73, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "light mode";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // btnUygula
+            // 
+            this.btnUygula.Location = new System.Drawing.Point(34, 146);
+            this.btnUygula.Name = "btnUygula";
+            this.btnUygula.Size = new System.Drawing.Size(172, 36);
+            this.btnUygula.TabIndex = 2;
+            this.btnUygula.Text = "uygula";
+            this.btnUygula.UseVisualStyleBackColor = true;
+            this.btnUygula.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(306, 324);
+            this.Controls.Add(this.btnUygula);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.Button btnUygula;
+    }
+}
+
---
+++ odev4-847/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev4_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rb1.Checked == true)
+            {
+                this.BackColor = Color.Black;
+                rb1.ForeColor = Color.White;
+                rb2.ForeColor = Color.White;
+            }
+                
+            if(!rb1.Checked == true)
+            {      
+                this.BackColor = Color.White;
+                rb1.ForeColor = Color.Black;
+                rb2.ForeColor = Color.Black;
+            }
+        }
+    }
+}
